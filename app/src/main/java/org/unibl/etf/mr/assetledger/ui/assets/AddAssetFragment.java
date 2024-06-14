0dcc8469fdf2a9eb8304cff68e275544a81f414d
MarioPerac <peracmario@outlook.com>
2024-06-14 11:12:12 +0200

    add button for barcode scanning

--- app/src/main/java/org/unibl/etf/mr/assetledger/ui/assets/AddAssetFragment.java
+++ app/src/main/java/org/unibl/etf/mr/assetledger/ui/assets/AddAssetFragment.java
@@ -2,6 +2,8 @@ package org.unibl.etf.mr.assetledger.ui.assets;
 
 import static android.app.Activity.RESULT_OK;
 
+import static androidx.core.content.ContentProviderCompat.requireContext;
+
 import android.Manifest;
 import android.content.ContentValues;
 import android.content.Intent;
@@ -12,6 +14,9 @@ import android.os.Bundle;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
@@ -23,26 +28,58 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.github.dhaval2404.imagepicker.ImagePicker;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 import org.unibl.etf.mr.assetledger.R;
+import org.unibl.etf.mr.assetledger.assetsdb.AssetDatabase;
+import org.unibl.etf.mr.assetledger.assetsdb.dao.AssetDAO;
+import org.unibl.etf.mr.assetledger.model.AssetInfo;
 
+import java.io.File;
 import java.io.OutputStream;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import android.content.pm.PackageManager;
+import android.widget.Toast;
 
 
 public class AddAssetFragment extends Fragment {
 
     ImageView assetImage;
-    private ActivityResultLauncher<Intent> imagePickerLauncher;
+
+    Button buttonAdd;
+
+    AssetDAO assetDAO;
+    Button buttonScan;
+
+    EditText name;
+    EditText description;
+
+    EditText employeeName;
+
+    EditText barcode;
+    EditText price;
+
 
     private static int CAMERA_PERMISSION_CODE = 1;
-    private static int READ_EXTERNAL_STORAGE_PERMISSION_CODE = 2;
+    private static int READ_MEDIA_IMAGES = 2;
 
+
+    private final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(),
+            result -> {
+                if (result.getContents() != null) {
+                    barcode.setText(result.getContents());
+                }
+            });
 
     public AddAssetFragment() {
         // Required empty public constructor
@@ -52,7 +89,7 @@ public class AddAssetFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+        assetDAO = AssetDatabase.getInstance(getContext()).getAssetDAO();
 
     }
 
@@ -65,6 +102,21 @@ public class AddAssetFragment extends Fragment {
         assetImage = root.findViewById(R.id.imageViewAddAsset);
         FloatingActionButton addImage = root.findViewById(R.id.addAssetImageButton);
 
+        buttonAdd = root.findViewById(R.id.buttonAdd);
+        buttonScan = root.findViewById(R.id.buttonScan);
+        name = root.findViewById(R.id.editTextName);
+        description = root.findViewById(R.id.editTextDescription);
+        employeeName = root.findViewById(R.id.editTextEmployeeName);
+        barcode = root.findViewById(R.id.editTextBarcode);
+        price = root.findViewById(R.id.editTextPrice);
+
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onScanButtonClick(v);
+            }
+        });
+
         addImage.setOnClickListener(new View.OnClickListener() {
 
 
@@ -73,11 +125,7 @@ public class AddAssetFragment extends Fragment {
                 if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
                     ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
                 } else {
-                    ImagePicker.with(AddAssetFragment.this)
-                            .crop()                    //Crop image(Optional), Check Customization for more option
-                            .compress(1024)            //Final image size will be less than 1 MB(Optional)
-                            .maxResultSize(500, 500)    //Final image resolution will be less than 1080 x 1080(Optional)
-                            .start();
+
                 }
             }
         });
@@ -85,6 +133,7 @@ public class AddAssetFragment extends Fragment {
 
         return root;
     }
+
 
     private void saveImageToGallery(Uri fileUri) {
         try {
@@ -107,4 +156,34 @@ public class AddAssetFragment extends Fragment {
         }
     }
 
+
+    public void onAddButtonClick(View view) {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+
+                //izvrsi logiku
+
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+//                        if (assetInfoList.isEmpty()) {
+//                            emptyListMessage.setVisibility(View.VISIBLE);
+//                        } else {
+//                            emptyListMessage.setVisibility(View.GONE);
+//                        }
+                    }
+                });
+            }
+        });
+    }
+
+    public void onScanButtonClick(View view) {
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Scan a barcode");
+        options.setOrientationLocked(false);
+        options.setBeepEnabled(false);
+        barcodeLauncher.launch(options);
+    }
 }
