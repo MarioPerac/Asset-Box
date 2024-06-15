2bd0054c1cb49f27119ecf6ea03c8f539e8097b6
MarioPerac <peracmario@outlook.com>
2024-06-15 18:46:25 +0200

    add search bar in assets fragment

--- app/src/main/java/org/unibl/etf/mr/assetledger/ui/assets/AssetsFragment.java
+++ app/src/main/java/org/unibl/etf/mr/assetledger/ui/assets/AssetsFragment.java
@@ -1,18 +1,22 @@
 package org.unibl.etf.mr.assetledger.ui.assets;
 
-import android.content.Context;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -23,87 +27,119 @@ import org.unibl.etf.mr.assetledger.model.Asset;
 import org.unibl.etf.mr.assetledger.model.AssetInfo;
 import org.unibl.etf.mr.assetledger.recyclerview.AssetInfoRecyclerViewAdapter;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class AssetsFragment extends Fragment {
 
+    private List<AssetInfo> assetInfoList = new ArrayList<>();
+    private List<AssetInfo> filteredAssetInfoList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private AssetDAO assetDAO;
+    private AssetInfoRecyclerViewAdapter adapter;
+    private Spinner searchCategorySpinner;
+    private SearchView searchView;
+    private String currentSearchCategory = "Name";
 
-    List<AssetInfo> assetInfoList;
-    RecyclerView recyclerView;
-    AssetDAO assetDAO;
-
-    TextView emptyListMessage;
-
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
     public AssetsFragment() {
     }
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         assetDAO = AssetDatabase.getInstance(getContext()).getAssetDAO();
         if (getArguments() != null) {
-            assetInfoList = (List<AssetInfo>) getArguments().getSerializable("assets");
+            assetInfoList = (ArrayList<AssetInfo>) getArguments().getSerializable("assets");
+            filteredAssetInfoList.addAll(assetInfoList);
         }
-
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_assets_list, container, false);
 
-        Context context = view.getContext();
-        recyclerView = (RecyclerView) view.findViewById(R.id.assets_list);
-        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        recyclerView = view.findViewById(R.id.assets_list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new AssetInfoRecyclerViewAdapter(filteredAssetInfoList, this::onAssetClick);
+        recyclerView.setAdapter(adapter);
 
-        recyclerView.setAdapter(new AssetInfoRecyclerViewAdapter(assetInfoList, this::onAssetClick));
+        searchCategorySpinner = view.findViewById(R.id.search_category_spinner);
+        searchView = view.findViewById(R.id.search_view);
 
-        emptyListMessage = view.findViewById(R.id.emptyListMessage);
+        ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.search_categories, android.R.layout.simple_spinner_item);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        searchCategorySpinner.setAdapter(spinnerAdapter);
+        searchCategorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                currentSearchCategory = parent.getItemAtPosition(position).toString();
+                filterAssets(searchView.getQuery().toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                filterAssets(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filterAssets(newText);
+                return false;
+            }
+        });
+
         FloatingActionButton addAssetButton = view.findViewById(R.id.addAssetButton);
         addAssetButton.setOnClickListener(this::onAddAssetClick);
 
-        if (assetInfoList.isEmpty()) {
+        TextView emptyListMessage = view.findViewById(R.id.emptyListMessage);
+        if (filteredAssetInfoList.isEmpty()) {
             emptyListMessage.setVisibility(View.VISIBLE);
         } else {
             emptyListMessage.setVisibility(View.GONE);
         }
+
         return view;
     }
 
-
-    public void onAddAssetClick(View view) {
+    private void onAddAssetClick(View view) {
         Navigation.findNavController(view).navigate(R.id.action_navigation_assets_to_addAssetFragment);
     }
 
-    public void onAssetClick(View view, AssetInfo assetInfo) {
+    private void onAssetClick(View view, AssetInfo assetInfo) {
         ExecutorService executorService = Executors.newSingleThreadExecutor();
-        executorService.execute(new Runnable() {
-            @Override
-            public void run() {
-                Asset asset = assetDAO.getById(assetInfo.getId());
-
-
-                getActivity().runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Bundle bundle = new Bundle();
-                        bundle.putSerializable("asset", asset);
-                        Navigation.findNavController(view).navigate(R.id.action_navigation_assets_to_assetDetailsFragment, bundle);
-                    }
-                });
-            }
+        executorService.execute(() -> {
+            Asset asset = assetDAO.getById(assetInfo.getId());
+            getActivity().runOnUiThread(() -> {
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("asset", asset);
+                Navigation.findNavController(view).navigate(R.id.action_navigation_assets_to_assetDetailsFragment, bundle);
+            });
         });
+    }
 
+    private void filterAssets(String query) {
+        filteredAssetInfoList.clear();
+        if (TextUtils.isEmpty(query)) {
+            filteredAssetInfoList.addAll(assetInfoList);
+        } else {
+            for (AssetInfo assetInfo : assetInfoList) {
+                if (currentSearchCategory.equals("Name") && assetInfo.getName().toLowerCase().contains(query.toLowerCase())) {
+                    filteredAssetInfoList.add(assetInfo);
+                } else if (currentSearchCategory.equals("Location") && assetInfo.getLocation().toLowerCase().contains(query.toLowerCase())) {
+                    filteredAssetInfoList.add(assetInfo);
+                }
+            }
+        }
+        adapter.notifyDataSetChanged();
     }
 }
-
